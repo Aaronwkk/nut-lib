9a0f3d571defc9ef01d404774500c06410e9ad92
Moon <p_winner1991@163.com>
2020-06-19 14:35:48 +0800

    feat(pkg): 包更新

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,53 +1,53 @@
 module.exports = {
-  "parser": "vue-eslint-parser",
-  "parserOptions": {
-    "parser": "babel-eslint",
-    "sourceType": "module",
-    "allowImportExportEverywhere": false,
+  'parser': 'vue-eslint-parser',
+  'parserOptions': {
+    'parser': 'babel-eslint',
+    'sourceType': 'module',
+    'allowImportExportEverywhere': false,
   },
   // "extends": [
   //   require.resolve('dev-hook-utils/dist/hooks/eslintrc.js')
   // ],
-  "globals": {
-    "APP_VERSION": true,
-    "$": true,
-    "Cropper": true,
-    "angular": true,
-    "describe": true,
-    "it": true,
-    "expect": true,
-    "beforeEach": true,
-    "afterEach": true
+  'globals': {
+    'APP_VERSION': true,
+    '$': true,
+    'Cropper': true,
+    'angular': true,
+    'describe': true,
+    'it': true,
+    'expect': true,
+    'beforeEach': true,
+    'afterEach': true
   },
-  "rules": {
-    "no-param-reassign": [
-      "error",
+  'rules': {
+    'no-param-reassign': [
+      'error',
       {
-        "props": false
+        'props': false
       }
     ],
-    "quotes": [
+    'quotes': [
       2,
-      "single",
+      'single',
       {
-        "avoidEscape": true,
-        "allowTemplateLiterals": true
+        'avoidEscape': true,
+        'allowTemplateLiterals': true
       }
     ],
-    "import/no-unresolved": [
-      "off"
+    'import/no-unresolved': [
+      'off'
     ],
-    "angular/angularelement": 0,
-    "angular/document-service": 0,
-    "angular/window-service": 0,
-    "no-underscore-dangle": 0,
-    "global-require": 0,
-    "import/extensions": ['off', 'never'],
-    "comma-dangle": [
-      "error",
-      "only-multiline"
+    'angular/angularelement': 0,
+    'angular/document-service': 0,
+    'angular/window-service': 0,
+    'no-underscore-dangle': 0,
+    'global-require': 0,
+    'import/extensions': ['off', 'never'],
+    'comma-dangle': [
+      'error',
+      'only-multiline'
     ],
-    "indent": ["error", 2, { "SwitchCase": 1 }],
-    "class-methods-use-this": 0,
+    'indent': ['error', 2, { 'SwitchCase': 1 }],
+    'class-methods-use-this': 0,
   }
 };
